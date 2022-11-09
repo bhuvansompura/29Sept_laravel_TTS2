f17f89435f3ddca4af9cfd2a9779f7174ffea4ef
Bhuvan Sompura <114719140+bhuvansompura@users.noreply.github.com>
2022-11-09 20:00:06 +0530

    Add files via upload

---
+++ php/03condition.php
@@ -0,0 +1,33 @@
+<?php
+
+$day = "sunday";
+
+if ($day== "sunday") {
+    echo "today is holiday";
+}else{
+echo "todai is not holiday";
+
+}
+ $marks = "90";
+ if ($marks >= 50) {
+    echo "pass";
+
+ } 
+
+ $number = "75";
+ if ($number >= 50) {
+    echo "B grad";
+ }
+ else if ($number > 70 & $number > 80)
+    echo "A grad";
+
+    else {
+        if ($number = 90)
+        echo "A+ grad";
+    }
+ 
+
+    
+
+
+?>
